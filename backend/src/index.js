3db1243269df47a735438551324596425a671b37
JustEd2032 <giogargarcia@gmail.com>
2026-02-08 18:58:11 -0600

    added root /

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Lawyer API is running 🚀");
+});
+
 app.get("/health", (req, res) => {
   res.send("API is healthy");
 });
